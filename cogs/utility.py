845eee6222e2d54c56b46daa31db8aa25e090c02
cmyui <cmyuiosu@gmail.com>
2020-06-30 14:28:32 -0400

    brain is suuuuuper used to writing packets and osu server but not so much discord bots lol

--- cogs/utility.py
+++ cogs/utility.py
@@ -6,16 +6,16 @@ from discord.ext import commands
 from time import time
 
 from Aika import ContextWrap
+from utils import seconds_readable
 
 class Utility(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    from utils import seconds_readable()
     @commands.command()
     @commands.cooldown(3, 5, commands.BucketType.user)
     async def uptime(self, ctx: ContextWrap) -> None:
-        uptime = seconds_readable(time() - self.bot.uptime)
+        uptime = seconds_readable(int(time() - self.bot.uptime))
         await ctx.send(f"I've been running for **{uptime}**.")
 
     @commands.command(hidden = True)
